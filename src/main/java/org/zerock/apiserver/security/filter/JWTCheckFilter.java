dc248e1e6eeabdbfa51c4848dec060ac2d19cfd3
pyjavathon <ayeonyun391@gmail.com>
2024-04-25 17:14:26 +0900

    결제 초안

--- src/main/java/org/zerock/apiserver/security/filter/JWTCheckFilter.java
+++ src/main/java/org/zerock/apiserver/security/filter/JWTCheckFilter.java
@@ -30,6 +30,7 @@ public class JWTCheckFilter extends OncePerRequestFilter {
 
         if(path.startsWith("/api/member/")) return true;
         if(path.startsWith("/api/products/view/")) return true;
+        if(path.startsWith("/api/pay/")) return true;
         //false == check
         return false;
     }
